46379a9e7cc5cca86c196fa0fcd1a204ad07d374
Camron Withrow <cmwithrow9@gmail.com>
2023-04-19 15:20:31 -0400

    written tip and cost-per-person functions

--- main.js
+++ main.js
@@ -5,19 +5,23 @@ const tipButtons = tipPercentage.map((percentage) => {
   return document.querySelector(`#${id}`);
 });
 
-// this should return the tip amount *and* total amount together
-function calculateTip(billTotal, percentage) {
-  // currently can round down... need to check to make sure that doesn't happen
-  return (billTotal * (1 + percentage / 100)).toFixed(2);
+// directly update the entries
+function updateCosts(percentage) {
+  const billTotal = document.querySelector(".bill").value;
+  const partySize = document.querySelector(".party-size").value;
+  // we calculate in terms of cents (not dollars) since pennies are atomic
+  let tipPerPerson =
+    Math.ceil(Math.ceil(billTotal * percentage) / partySize) / 100;
+  let costPerPerson =
+    Math.ceil((100 * billTotal) / partySize) / 100 + tipPerPerson;
+  document.querySelector("#tip-per-person").textContent =
+    tipPerPerson.toFixed(2);
+  document.querySelector("#total-per-person").textContent =
+    costPerPerson.toFixed(2);
 }
 
-// attach listeners to each button that modifies the display
 for (let i = 0; i < tipButtons.length; i++) {
   const button = tipButtons[i];
   const percentage = tipPercentage[i];
-  button.addEventListener("click", () => {
-    const billTotal = document.querySelector(".bill").value;
-    let billPlusTip = calculateTip(billTotal, percentage);
-    document.querySelector("#total-per-person").textContent = billPlusTip;
-  });
+  button.addEventListener("click", () => updateCosts(percentage));
 }
