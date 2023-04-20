3c58ed42d902b35735afa81a40c050f4aab30c74
Camron Withrow <cmwithrow9@gmail.com>
2023-04-20 11:13:18 -0400

    rearrange javascript file

--- main.js
+++ main.js
@@ -1,26 +1,8 @@
 const tip = document.querySelector("#tip-per-person");
 const cost = document.querySelector("#total-per-person");
 
-function resetResults() {
-  tip.textContent = `$0.00`;
-  cost.textContent = `$0.00`;
-}
-
-// directly update the entries
-function updateCosts(percentage) {
-  const billTotal = document.querySelector("#bill").value;
-  const partySize = document.querySelector("#party-size").value;
-  // we calculate in terms of cents (not dollars) since pennies are atomic
-  let tipPerPerson =
-    Math.ceil(Math.ceil(billTotal * percentage) / partySize) / 100;
-  let costPerPerson =
-    Math.ceil((100 * billTotal) / partySize) / 100 + tipPerPerson;
-  tip.textContent = `$${tipPerPerson.toFixed(2)}`;
-  cost.textContent = `$${costPerPerson.toFixed(2)}`;
-}
-
-//construct array of tip buttons
 const tipPercentage = [5, 10, 15, 25, 50];
+// zip tipPercentage with the tip radio buttons
 const tipButtons = tipPercentage.map((percentage) => {
   var id = `#tip${percentage}`;
   return { p: percentage, button: document.querySelector(`${id}`) };
@@ -30,6 +12,22 @@ const bill = document.querySelector("#bill");
 const partySize = document.querySelector("#party-size");
 const radioButtons = document.querySelectorAll(".tip-button");
 const inputElements = [bill, partySize];
+
+function resetResults() {
+  tip.textContent = `$0.00`;
+  cost.textContent = `$0.00`;
+}
+
+// directly update the entries
+function updateCosts(percentage) {
+  let billTotal = bill.value;
+  let count = partySize.value;
+  // we calculate in terms of cents (not dollars) since pennies are atomic
+  let tipPerPerson = Math.ceil(Math.ceil(billTotal * percentage) / count) / 100;
+  let costPerPerson = Math.ceil((100 * billTotal) / count) / 100 + tipPerPerson;
+  tip.textContent = `$${tipPerPerson.toFixed(2)}`;
+  cost.textContent = `$${costPerPerson.toFixed(2)}`;
+}
 
 function inputValidates() {
   // check each input field against the appropriate regular expression
