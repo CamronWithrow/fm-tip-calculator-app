ad587236cde5c2b648bc0534e9d2e1c114878469
Camron Withrow <cmwithrow9@gmail.com>
2023-04-19 20:04:23 -0400

    refactored javascript functionality

--- main.js
+++ main.js
@@ -1,36 +1,53 @@
-//construct array of tip buttons
-const tipPercentage = [5, 10, 15, 25, 50, "custom"];
-const tipButtons = tipPercentage.map((percentage) => {
-  if (percentage === "custom") {
-    // currently doesn't update at the right time...
-    // need to click on the input field again after the button is selected and the field contains a value
-    var id = ".custom-tip";
-  } else {
-    var id = `#tip${percentage}`;
-  }
-  return document.querySelector(`${id}`);
-});
+function isValidEntry(htmlInput) {
+  return htmlInput.value != "";
+}
+
+function resetResults() {
+  document.querySelector("#tip-per-person").textContent = `$0.00`;
+  document.querySelector("#total-per-person").textContent = `$0.00`;
+}
 
 // directly update the entries
 function updateCosts(percentage) {
-  if (percentage === "custom") {
-    percentage = document.querySelector(".custom-tip").value;
-  }
-  const billTotal = document.querySelector(".bill").value;
-  const partySize = document.querySelector(".party-size").value;
+  const billTotal = document.querySelector("#bill").value;
+  const partySize = document.querySelector("#party-size").value;
   // we calculate in terms of cents (not dollars) since pennies are atomic
   let tipPerPerson =
     Math.ceil(Math.ceil(billTotal * percentage) / partySize) / 100;
   let costPerPerson =
     Math.ceil((100 * billTotal) / partySize) / 100 + tipPerPerson;
-  document.querySelector("#tip-per-person").textContent =
-    tipPerPerson.toFixed(2);
-  document.querySelector("#total-per-person").textContent =
-    costPerPerson.toFixed(2);
+  document.querySelector(
+    "#tip-per-person"
+  ).textContent = `$${tipPerPerson.toFixed(2)}`;
+  document.querySelector(
+    "#total-per-person"
+  ).textContent = `$${costPerPerson.toFixed(2)}`;
 }
 
-for (let i = 0; i < tipButtons.length; i++) {
-  const button = tipButtons[i];
-  const percentage = tipPercentage[i];
-  button.addEventListener("click", () => updateCosts(percentage));
+//construct array of tip buttons
+const tipPercentage = [5, 10, 15, 25, 50];
+const tipButtons = tipPercentage.map((percentage) => {
+  var id = `#tip${percentage}`;
+  return { p: percentage, button: document.querySelector(`${id}`) };
+});
+
+const inputElements = document.querySelectorAll(".text-input");
+const radioButtons = document.querySelectorAll(".tip-button");
+
+function attemptUpdate() {
+  for (const input of inputElements) {
+    if (!isValidEntry(input)) {
+      return resetResults();
+    }
+  }
+  let { p, _ } = tipButtons.find(({ _, button }) => button.checked);
+  updateCosts(p);
 }
+
+inputElements.forEach((element) => {
+  element.addEventListener("change", attemptUpdate);
+});
+
+radioButtons.forEach((button) => {
+  button.addEventListener("click", attemptUpdate);
+});
